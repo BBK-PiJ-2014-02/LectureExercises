93a28eceb3851d14f67a04f060b9e5ee2142a7e4
vdiasf01 <vascohortalondres@sapo.pt>
2014-11-07 21:10:46 +0000

    Day 6 - Exercise 3 - Creating matrices - complete

--- 2014-11-03 - Day 06/03 Creating Matrices/Matrix.java
+++ 2014-11-03 - Day 06/03 Creating Matrices/Matrix.java
@@ -4,39 +4,124 @@ public class Matrix {
 
     private int[][] matrix;
 
-    public Matrix(int aSize, int bSize) {
-        this.matrix = new int[aSize][bSize];
+    // Constructor
+    public Matrix(int xSize, int ySize) {
+        this.matrix = new int[ySize][xSize];
         reset();
-    }
+	}
 
     // Sets all elements of the matrix to 1
     private void reset() {
-        for( int i = 0; i < this.matrix.length; i++ ) {
-            for( int j = 0; j < this.matrix[i].length; j++ ) {
-                this.matrix[i][j] = 1;
+        for( int y = 0; y < this.matrix.length; y++ ) {
+            for( int x = 0; x < this.matrix[y].length; x++ ) {
+                this.matrix[y][x] = 1;
             }
         }
     }
 
+    // Set one element of the matrix
     public void setElement(int x, int y, int value) {
-        this.matrix[x][y] = value;
+		// Check if x,y values are inside the bounds
+		if ( x >= 0 && y >= 0 && y < this.matrix.length && x < matrix[this.matrix.length-1].length ) {
+            this.matrix[y][x] = value;
+        }
     }
 
-    public void setRow(int x, String str) {
+    // Set a row with comma separated string values
+    public void setRow(int y, String str) {
+		if ( str == null ) {
+			return;
+        }
+		int strNumbers = commaCount(str) + 1;
+
+		// Ensure that the row to update is valid
+        if ( y >= 0 && y < this.matrix.length && strNumbers == this.matrix[0].length ) {
+			int[] numbers = new int[strNumbers];
+			numbers = toInt(str);
+			for ( int i = 0; i < strNumbers; i++ ) {
+				setElement(i,y,numbers[i]);
+			}
+        }
     }
 
+    // Set a column with comma separated string values
     public void setColumn(int x, String str) {
+		if ( str == null ) {
+			return;
+        }
+		int strNumbers = commaCount(str) + 1;
+
+		// Ensure that the column to update is valid
+        if ( x >= 0 && x < this.matrix[0].length && strNumbers == this.matrix.length ) {
+			int[] numbers = new int[strNumbers];
+			numbers = toInt(str);
+			for ( int i = 0; i < strNumbers; i++ ) {
+				setElement(x,i,numbers[i]);
+			}
+        }
     }
+
+    // Count number of commas in string
+    // Internal functionality not to be exposed to other classes
+    private int commaCount(String str) {
+        int commas = 0;
+        for( int i = 0; i < str.length(); i++ ) {
+            if ( str.charAt(i) == ',' ) {
+                commas++;
+            }
+		}
+		return commas;
+	}
+
+    // Convert "1,2,3,4" to int array with 1,2,3,4
+    // Internal functionality not to be exposed to other classes
+    private int[] toInt(String str) {
+        int[] integers;
+        integers = new int[commaCount(str)+1];
+
+        int index = 0;
+
+        String s = "";
+        for( int i = 0; i < str.length(); i++ ) {
+            if ( str.charAt(i) != ',' ) {
+                s += str.charAt(i);
+            }
+            else {
+				integers[index] = Integer.parseInt(s);
+				s = "";
+				index++;
+			}
+		}
+        integers[index] = Integer.parseInt(s);
+
+        return integers;
+	}
 
     @Override
+    // Converting the matrix to String i.e.: [1,2,-2;2,3,0..]
     public String toString() {
-        return "";
+		String str = "[";
+		for( int y = 0; y < this.matrix.length; y++ ) {
+			String row = "";
+			for( int x = 0; x < this.matrix[0].length; x++ ) {
+                row += this.matrix[y][x];
+                if ( x+1 != this.matrix[0].length ) {
+				    row += ",";
+				}
+			}
+			str += row;
+			if ( y+1 != this.matrix.length ) {
+				str += ";";
+            }
+		}
+		str += "]";
+        return str;
     }
 
     public void prettyPrint() {
-        for( int i = 0; i < this.matrix.length; i++ ) {
-            for( int j = 0; j < this.matrix[i].length; j++ ) {
-                System.out.print("\t"+this.matrix[i][j]);
+        for( int y = 0; y < this.matrix.length; y++ ) {
+            for( int x = 0; x < this.matrix[y].length; x++ ) {
+                System.out.print("\t"+this.matrix[y][x]);
             }
             System.out.println();
         }
--- 2014-11-03 - Day 06/03 Creating Matrices/MatrixTester.java
+++ 2014-11-03 - Day 06/03 Creating Matrices/MatrixTester.java
@@ -3,9 +3,12 @@
 public class MatrixTester {
 
   public static void main( String[] args ) {
-    Matrix m = new Matrix(8,8);
-    m.setElement(2,3,4);
+    Matrix m = new Matrix(5,6);
+    m.setElement(3,2,4);
+    m.setRow(3,"-2,4,3,43,44");
+    m.setColumn(4,"-4,4,3,5,6,7");
+    String str = m.toString();
     m.prettyPrint();
-
+    System.out.println(str);
   }
 }
