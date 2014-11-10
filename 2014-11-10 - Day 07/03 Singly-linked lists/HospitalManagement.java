5c5fb53c180d4c4f1625f4ee2d8713698b9770e5
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2014-11-10 18:39:47 +0000

    Day 7 - Exercise 3 - Singly-linked lists - completed

--- 2014-11-10 - Day 07/03 Singly-linked lists/HospitalManagement.java
+++ 2014-11-10 - Day 07/03 Singly-linked lists/HospitalManagement.java
@@ -1,33 +1,33 @@
 /** Day 7 - Exercise 3 - Singly-linked lists **/
 
 public class HospitalManagement {
-    private Patient firstPatient;
-    private Patient pointer;
+    private Patient firstPatient = null;
 
-	public void addPatient( Patient patient ) {
-		if ( this.firstPatient != null ) {
-			if ( pointer.getNextPatient() != null ) {
-			patient.print();
-				addPatient(pointer.getNextPatient());
-			}
-			else {
-				patient.setNextPatient(patient);
-			}
+    // Add Patient p to the list
+    public void addPatient(Patient p) {
+		// If this is the first element
+		if ( this.firstPatient == null ) {
+			this.firstPatient = p;
 		}
+		// When not first element
 		else {
-			this.firstPatient = patient;
-			this.pointer = this.firstPatient;
+			this.firstPatient.addPatient(p);
 		}
 	}
 
-	public void remove( Patient patient ) {
-
+    // Delete a Patient by name
+	public void deletePatient(String name) {
+		// When first element is the one we need to delete
+		if ( this.firstPatient.getName().equals(name) ) {
+			this.firstPatient = this.firstPatient.nextPatient();
+		}
+		// When next is not the one to delete yet
+		else {
+	    	this.firstPatient.deletePatient(name);
+	    }
 	}
 
-    public Patient getNext(Patient previousPatient) {
-		return previousPatient.getNextPatient();
-	}
-
+    // Print the whole list
 	public void printList() {
         Patient p = this.firstPatient;
 		if ( p == null ) {
@@ -36,10 +36,8 @@ public class HospitalManagement {
 		else {
             do{
     			p.print();
-    			p = p.getNextPatient();
+    			p = p.nextPatient();
     		} while (p != null);
 		}
 	}
-
-
 }
--- 2014-11-10 - Day 07/03 Singly-linked lists/Patient.java
+++ 2014-11-10 - Day 07/03 Singly-linked lists/Patient.java
@@ -1,25 +1,59 @@
 /** Day 7 - Exercise 3 - Singly-linked lists **/
 
 public class Patient {
-	private String name;
-	private int age;
-	private String illness;
+	private String  name;
+	private int     age;
+	private String  illness;
 	private Patient nextPatient;
 
     public Patient(String name, int age, String illness) {
-	    this.name = name;
-	    this.age = age;
-	    this.illness = illness;
+	    this.name        = name;
+	    this.age         = age;
+	    this.illness     = illness;
+	    this.nextPatient = null;
 	}
 
-	public Patient getNextPatient() {
+    // Add a patient to the list
+	public void addPatient(Patient patient) {
+		// Nothing to add.
+		if ( patient == null ) {
+			return;
+		}
+
+        // End of the list, add this patient
+		if ( this.nextPatient == null ) {
+			this.nextPatient = patient;
+		}
+		// Not yet the end, keep searching
+		else {
+			this.nextPatient.addPatient(patient);
+		}
+	}
+
+    // Delete a patient from the list by name
+    public void deletePatient(String name) {
+        // When next patient is the one we want to delete
+		if ( this.nextPatient != null && this.nextPatient.getName().equals(name)) {
+			this.nextPatient = this.nextPatient.nextPatient;
+
+        // While we have not reached the end of the list
+		} else if ( this.nextPatient != null ) {
+			this.nextPatient.deletePatient(name);
+		}
+		// Not found => nothing deleted
+	}
+
+    // Method to access the private this next patient
+    public Patient nextPatient() {
 		return this.nextPatient;
 	}
 
-	public void setNextPatient(Patient nextPatient) {
-		this.nextPatient = nextPatient;
+    // Mathod to get the patient's name for comparison when deleting
+    public String getName() {
+		return this.name;
 	}
 
+    // Print this Patient's info
 	public void print() {
 		System.out.println("Patient: " + name + ", age " + age + ", illness: " + illness);
 	}
