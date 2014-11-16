8fd62fb514694c15929a702e05f78bc8592a1568
vdiasf01 <vascohortalondres@sapo.pt>
2014-11-16 13:40:35 +0000

    Day 7 - Exercise 12 - Cocktail sort - complete

---
+++ 2014-11-10 - Day 07/12 Cocktail sort/ListUtilities.java
@@ -0,0 +1,29 @@
+/** Day 7 - Exercise 12 - Cocktail sort **/
+
+public class ListUtilities {
+
+    // Static public method to convert int arrays into
+    // a list of type ListManager
+    public static ListManager toList(int[] intArray) {
+		ListManager lm = new ListManager();
+		IntElement ie;
+
+		for( int i = 0; i < intArray.length; i++ ) {
+			ie = new IntElement(intArray[i]);
+			lm.add(ie);
+		}
+		return lm;
+	}
+
+    // Static public method to bubble sort a ListManager list
+    // into a sorted list of type ListManager
+    public static void bubbleSort(ListManager list) {
+        list.bubbleSort();
+    }
+
+    // Static public method to cocktail sort a ListManager list
+    // into a sorted list of type ListManager
+    public static void cocktailSort(ListManager list) {
+        list.cocktailSort();
+    }
+}
---
+++ 2014-11-10 - Day 07/12 Cocktail sort/TestCocktailSort.java
@@ -0,0 +1,24 @@
+/** Day 7 - Exercise 12 - Cocktail sort **/
+
+public class TestCocktailSort {
+    public static void main( String[] args ) {
+		System.out.println("==== Welcome to the sorted ListUtilities World ===");
+
+        int[] intArray = {
+			6,2,12,33,5,56,67,45,45,6,546,45,657,567,54,43,5,46,76,567,56,756,
+			75,67,567,5,4,7,3,8,2,9,1,15,12,14,13,11,45,23,35,345,56,476,5,457,
+			56,54,645,76,456,456,456,45,645,34,45,45,6,45,45,7,56,54,6,456,456
+		};
+
+        ListManager listM = ListUtilities.toList(intArray);
+        listM.printList();
+
+        long start = System.currentTimeMillis();
+        ListUtilities.cocktailSort(listM);
+        long finish = System.currentTimeMillis();
+
+        System.out.println("It took: "+(finish-start)+"ms");
+        listM.printList();
+
+	}
+}
