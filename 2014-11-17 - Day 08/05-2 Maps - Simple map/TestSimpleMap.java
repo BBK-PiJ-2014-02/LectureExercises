ec95e45c627af33b2bd944a75271b5e3475f0377
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2014-11-17 20:59:26 +0000

    Day 8 - Exercise 5.2 - Simple Map - WIP

---
+++ 2014-11-17 - Day 08/05-2 Maps - Simple map/TestSimpleMap.java
@@ -0,0 +1,12 @@
+/** Day 8 - Exercise 5.1 - Hash function **/
+
+public class TestSimpleMap {
+    static public void main( String[] args ) {
+		SimpleMap sp = new SimpleMapImpl();
+		System.out.println("get: "+sp.get(0));
+		System.out.println("isEmpty: "+sp.isEmpty());
+//		System.out.println("get: "+sp.get(0));
+//		System.out.println("get: "+sp.get(0));
+
+	}
+}
