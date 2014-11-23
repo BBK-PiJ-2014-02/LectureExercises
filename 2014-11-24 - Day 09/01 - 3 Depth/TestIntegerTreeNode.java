1b06966a5abac0122a675900e14005f61c9a3398
vdiasf01 <vascohortalondres@sapo.pt>
2014-11-23 21:51:53 +0000

    Day 9 - Exercise 1.3 - Depth

---
+++ 2014-11-24 - Day 09/01 - 3 Depth/TestIntegerTreeNode.java
@@ -0,0 +1,20 @@
+/** Day 9 - Exercise 1.3 - Depth **/
+
+public class TestIntegerTreeNode {
+
+    public static void main( String[] args ) {
+		IntegerTreeNode itn = new IntegerTreeNodeImpl(6);
+		itn.add(9);
+		itn.add(5);
+		itn.add(3);
+		itn.add(8);
+		itn.add(11);
+		itn.add(12);
+
+        System.out.println("contains: "+itn.contains(145));
+        System.out.println("max: "+itn.getMax());
+        System.out.println("min: "+itn.getMin());
+        System.out.println(itn.toString());
+        System.out.println("Tree depth: "+itn.depth());
+	}
+}
