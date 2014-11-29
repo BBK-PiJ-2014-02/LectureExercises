66ffb9bbb11ac8ea756e4c8cdd4fcd0ab5424ae4
vdiasf01 <vascohortalondres@sapo.pt>
2014-11-29 10:54:59 +0000

    Day 9 - Exercise 3.3 - Trees as sorted lists - Implementation as list

--- 2014-11-24 - Day 09/03 - 3 Trees as sorted lists- Implementation as a list/ListIntSortedList.java
+++ 2014-11-24 - Day 09/03 - 3 Trees as sorted lists- Implementation as a list/ListIntSortedList.java
@@ -35,24 +35,37 @@ public class ListIntSortedList implements IntSortedList {
 	 *  A list can contain duplicated unlike a set.
 	 */
 	public void add(int i) {
+		// First element of the list start on the next of first elemt
+		// This is to allow the swap of the first with a smaller value
+		// without losing the reference of the first element.
 		if ( this.first && this.next == null ) {
 			this.next = new ListIntSortedList(i,false);
 		}
+		// We have a next element.
 		else {
+			// If we do not have a next element and this value is smaller,
+			// next will be a new element with this i value
     		if ( this.next == null && i >= this.value ) {
     			this.next = new ListIntSortedList(i,false);
     		}
+    		// If next is null and i is smaller than this value,
+    		// add a new element here and assign this element to the next
+    		// of the newly created element.
     		else if ( this.next == null && i < this.value ) {
     			ListIntSortedList tmp = new ListIntSortedList(i,false);
     			tmp.setNext(this);
     			this.next = null;
     			System.out.println("hey");
     		}
+    		// If this value is smaller than given i and i is also smaller than
+    		// the next element's value, a new element should be added between this
+    		// element and the next with the i value
     		else if ( this.value < i && i <= this.next.getValue() ) {
       			ListIntSortedList tmp = new ListIntSortedList(i,false);
      			tmp.setNext(this.next);
     			this.next = tmp;
     		}
+    		// For all other cases, recall add from the next element.
     		else {
     			this.next.add(i);
     		}
