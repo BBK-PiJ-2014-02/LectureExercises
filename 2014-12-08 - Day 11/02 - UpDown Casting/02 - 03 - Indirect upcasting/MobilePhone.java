92cc2ce4051a638183d804154798545382672df6
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2014-12-08 20:12:55 +0000

    Day 11 - Exercise 2.3 - completed

---
+++ 2014-12-08 - Day 11/02 - UpDown Casting/02 - 03 - Indirect upcasting/MobilePhone.java
@@ -0,0 +1,74 @@
+/**
+ * MobilePhone extends OldPhone
+ */
+public class MobilePhone extends OldPhone {
+	/**
+	 * Storing last ten numbers
+	 */
+	private String[] lastNumbers = new String[10];
+
+    /**
+     * PlayGame
+     * @param name String
+     */
+	public void playGame(String name) {
+		System.out.println("Playing game " + name);
+	}
+
+    /**
+     * ringAlarm
+     * @param alarm String
+     */
+	public void ringAlarm(String alarm) {
+		System.out.println("Alarm!");
+	}
+
+    /**
+     * printLastNumbers
+     */
+	public void printLastNumbers() {
+		for( int i = 0; i < lastNumbers.length; i++ ) {
+			System.out.println(i+ " : " + lastNumbers[i]);
+		}
+	}
+
+    /**
+     * call
+     * @param number String
+     */
+    @Override
+    public void call( String number ) {
+		super.call(number);
+        add(number);
+	}
+
+    /**
+     * add
+     * @param number adds number to memory
+     */
+	private void add(String number) {
+		if ( number != null ) {
+			// Move numbers down
+			for ( int i = lastNumbers.length - 1; i > 0; i-- ) {
+				lastNumbers[i] = lastNumbers[i-1];
+			}
+			lastNumbers[0] = number;
+		}
+	}
+
+	/**
+	 * browseWeb
+	 * @param str to browse the web.
+	 */
+    public void browseWeb(String str) {
+	}
+
+    /**
+     * findPosition returning GPS coordinates.
+     * @Return String position
+     */
+    public String findPosition() {
+		return null;
+	}
+
+}
---
+++ 2014-12-08 - Day 11/02 - UpDown Casting/02 - 03 - Indirect upcasting/PhoneLauncher.java
@@ -0,0 +1,32 @@
+/**
+ * PhoneLauncher to test the other classes
+ */
+public class PhoneLauncher {
+	/**
+	 * Main function
+	 * @param args String array
+	 */
+	public static void main(String[] args) {
+		PhoneLauncher launcher = new PhoneLauncher();
+		launcher.launch();
+	}
+
+    /**
+     * launch the SmartPhone class
+     */
+	public void launch() {
+		MobilePhone myPhone = new SmartPhone();
+		myPhone.browseWeb("Testing browseWeb()");
+		myPhone.findPosition();
+		testPhone(myPhone);
+	}
+
+	/**
+	 * testPhone();
+	 */
+	public void testPhone(Phone p) {
+		p.call("123");
+		// Cannot call this as it is not set on Phone interface
+		// p.findPosition();
+	}
+}
