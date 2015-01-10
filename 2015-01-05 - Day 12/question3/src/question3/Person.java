13d65979129b527d7e7fe1fdf264de526d840274
vdiasf01 <vascohortalondres@sapo.pt>
2015-01-10 17:52:47 +0000

    Question 3 completed.

--- 2015-01-05 - Day 12/question3/src/question3/Person.java
+++ 2015-01-05 - Day 12/question3/src/question3/Person.java
@@ -1,14 +1,29 @@
 package question3;
 
 public class Person {
-	
+	/**
+	 * Return the given full name initials.
+	 * 
+	 * @param fullName the person's full name
+	 * @return result
+	 */
 	public String getInitials(String fullName) {
+        // If fullName is null, return empty string to avoid exceptions.
+		if ( fullName == null ) {
+			return "";
+		}
+		
 		String result = "";
 		String[] words = fullName.split(" ");
+
 		for (int i = 0; i < words.length; i++) {
-			String nextInitial = "" + words[i].charAt(0);
-			result = result + nextInitial.toUpperCase();
+			// Double spaces would have returned an empty word
+			if ( words[i].length() > 0 ) {
+				String nextInitial = "" + words[i].charAt(0);
+				result = result + nextInitial.toUpperCase();
+			}
 		}
+
 		return result;
 	}
 }
--- 2015-01-05 - Day 12/question3/src/question3/PersonTest.java
+++ 2015-01-05 - Day 12/question3/src/question3/PersonTest.java
@@ -3,10 +3,17 @@ package question3;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
+/**
+ * Testing Person class
+ * @author Vasco
+ *
+ */
 public class PersonTest {
+	/**
+	 * Test Normal Name for correct initials.
+	 */
 	@Test
-	public void testsNoermalName() {
+	public void testsNormalName() {
 		Person p = new Person();
 		String input = "Dereck Robert Yasirt";
 		String output = p.getInitials(input);
@@ -14,4 +21,63 @@ public class PersonTest {
 		assertEquals(output, expected);
 	}
 
+	/**
+	 * Test on multiple spaces.
+	 */
+	@Test
+	public void testMultipleSpaces() {
+		Person p = new Person();
+		String input = "Dereck  Robert    Yasirt";
+		String output = p.getInitials(input);
+		String expected = "DRY";
+		assertEquals(output, expected);
+	}
+
+	/**
+	 * Test on upper cased initials.
+	 */
+	@Test
+	public void testUpperCases() {
+		Person p = new Person();
+		String input = "dereck    RObert   yasirt";
+		String output = p.getInitials(input);
+		String expected = "DRY";
+		assertEquals(output, expected);
+	}
+
+	/**
+	 * Test on an empty string name.
+	 */
+	@Test
+	public void testEmptyName() {
+		Person p = new Person();
+		String input = "";
+		String output = p.getInitials(input);
+		String expected = "";
+		assertEquals(output, expected);
+	}
+
+	/**
+	 * Test on a NULL name given.
+	 */
+	@Test
+	public void testNULLName() {
+		Person p = new Person();
+		String input = null;
+		String output = p.getInitials(input);
+		String expected = "";
+		assertEquals(output, expected);
+	}
+
+	/**
+	 * Test on one name only.
+	 */
+	@Test
+	public void testOneName() {
+		Person p = new Person();
+		String input = "oneName";
+		String output = p.getInitials(input);
+		String expected = "O";
+		assertEquals(output, expected);
+	}
 }
