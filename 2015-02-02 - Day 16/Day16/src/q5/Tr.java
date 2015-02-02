c1152942c323c96a4346ca194be928869fcff3ec
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-02-02 18:44:12 +0000

    Day 16 - q5 - tr - completed.

--- 2015-02-02 - Day 16/Day16/src/q5/Tr.java
+++ 2015-02-02 - Day 16/Day16/src/q5/Tr.java
@@ -16,68 +16,68 @@ import java.io.IOException;
  *
  */
 public class Tr {
-	/**
-	 * Main program.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Tr tr = new Tr();
-		tr.launch(args);
-	}
-	
-	/**
-	 * Launching the program.
-	 * 
-	 * @param args
-	 */
-	private void launch(String[] args) {
-		if ( args.length < 3 ) {
-			System.out.println("Invalid arguments.");
-			System.out.println("Please use Tr <file> <original word> <substituted word>");
-			return;
-		}
+    /**
+     * Main program.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Tr tr = new Tr();
+        tr.launch(args);
+    }
+    
+    /**
+     * Launching the program.
+     * 
+     * @param args
+     */
+    private void launch(String[] args) {
+        if ( args.length < 3 ) {
+            System.out.println("Invalid arguments.");
+            System.out.println("Please use Tr <file> <original word> <substituing word>");
+            return;
+        }
 
-		String file = args[0];
-		String word1 = args[1];
-		String word2 = args[2];
-		
-		printSubstitute(file, word1, word2);
-		
-	}
+        String file  = args[0];
+        String word1 = args[1];
+        String word2 = args[2];
+        
+        printSubstitute(file, word1, word2);
+        
+    }
 
-	/**
-	 * Substitute contents of the file 
-	 * where word1 is found, print word2.
-	 * 
-	 * @param fileName The file name
-	 * @param word1 word to be substituted
-	 * @param word2 word to be used instead
-	 */
-	private void printSubstitute(String fileName, String word1, String word2) {
-    	File file = new File(fileName);
+    /**
+     * Substitute contents of the file 
+     * where word1 is found, print word2.
+     * 
+     * @param fileName The file name
+     * @param word1 word to be substituted
+     * @param word2 word to be used instead
+     */
+    private void printSubstitute(String fileName, String word1, String word2) {
+        File file = new File(fileName);
 
-	    if ( file.exists() ) {
-        	BufferedReader fIn = null;
-		
-    		try {
-	    		fIn = new BufferedReader(new FileReader(fileName));
-		    	String line;
+        if ( file.exists() ) {
+            BufferedReader fIn = null;
+        
+            try {
+                fIn = new BufferedReader(new FileReader(fileName));
+                String line;
 
-	    		while(( line = fIn.readLine() ) != null ) {
-	    			line = line.replace(word1, word2);
-    				System.out.println(line);
-		    	}
+                while(( line = fIn.readLine() ) != null ) {
+                    line = line.replace(word1, word2);
+                    System.out.println(line);
+                }
 
-    		} catch (FileNotFoundException e) {
-	    		e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
 
-    		} catch (IOException e) {
-	    		e.printStackTrace();
-		    }
-    	
-    	} else {
-        	System.out.println("File '" + fileName + "' not found.");
-    	}
-	}
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        
+        } else {
+            System.out.println("File '" + fileName + "' not found.");
+        }
+    }
 }
