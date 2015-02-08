8ad4a35b924529e0febf7b94e165705bf0616f89
vdiasf01 <vascohortalondres@sapo.pt>
2015-02-08 23:02:31 +0000

    Day 17 - q1

---
+++ 2015-02-09 - Day 17/Day17/src/q1/TextLoop.java
@@ -0,0 +1,59 @@
+package q1;
+
+/**
+ * Question 1 Day 17
+ * 
+ * @author Vasco
+ *
+ */
+public class TextLoop implements Runnable {
+	public static final int COUNT = 10;
+	
+	private final String name;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 */
+	public TextLoop(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Runnable
+	 */
+	@Override
+	public void run() {
+		for( int i = 0; i < COUNT; i++ ) {
+			System.out.println("Loop: "+name+", iteration: "+i+".");
+		}
+	}
+
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] arg) {
+		String[] args = {"1"};
+		if(args.length < 1 || (!args[0].equals("0") && !args[0].equals("1"))) {
+			System.out.println("USAGE: java TextLoop <mode>");
+			System.out.println("       mode 0: without threads");
+			System.out.println("       mode 1: with threads");
+		}
+		else if (args[0].equals("0")) {
+			for ( int i = 0 ; i < 10; i++ ) {
+    			Runnable r = new TextLoop("Thread " + i ) ;
+	    		r.run();
+			}
+		}
+		else {
+			for ( int i = 0 ; i < 10; i++ ) {
+				Runnable r = new TextLoop("Thread " + i );
+				Thread t = new Thread(r);
+				t.start();
+			}
+		}
+	}
+}
