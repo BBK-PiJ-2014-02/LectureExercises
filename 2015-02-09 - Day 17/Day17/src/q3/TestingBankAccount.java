c66d0548b1680cd8b588e886fc9afd9a8df23141
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-02-09 21:32:35 +0000

    Day 17 - q3 - BankAccount

--- 2015-02-09 - Day 17/Day17/src/q3/TestingBankAccount.java
+++ 2015-02-09 - Day 17/Day17/src/q3/TestingBankAccount.java
@@ -6,7 +6,6 @@ package q3;
  *
  */
 public class TestingBankAccount implements Runnable {
-
 	/**
 	 * Customer's bank account.
 	 */
@@ -28,12 +27,20 @@ public class TestingBankAccount implements Runnable {
 	 */
 	public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
-        bankAccount.deposit(20);
-        for(int i = 0; i < 20; i++ ) {
+        bankAccount.deposit(200);
+        for(int i = 0; i < 200; i++ ) {
         	TestingBankAccount tba = new TestingBankAccount(bankAccount);
         	Thread            bank = new Thread(tba);
         	bank.start();
         }
+        try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("Final balance: " + bankAccount.getBalance());
+		
 	}
 
 	/**
@@ -41,11 +48,16 @@ public class TestingBankAccount implements Runnable {
 	 */
 	@Override
 	public void run() {
-		System.out.println("Starting a new thread. Current balance: " + bankAccount.getBalance());
-		for( int i = 0; i < 10; i++ ) {
+//		System.out.println("Starting a new thread. Current balance: " + bankAccount.getBalance());
+		for( int i = 0; i < 100; i++ ) {
 			bankAccount.retrieve(1);
-			System.out.println("    running " + i + " deposited: " + bankAccount.getBalance());
+//			System.out.println("    running " + i + " balance: " + bankAccount.getBalance());
 		}
-		System.out.println("Stopping this thread. Current balance: " + bankAccount.getBalance());
+//		System.out.println("Stopping this thread. Current balance: " + bankAccount.getBalance());
+		for( int i = 0; i < 100; i++ ) {
+			bankAccount.deposit(1);
+//			System.out.println("    running " + i + " balance: " + bankAccount.getBalance());
+		}
+//		System.out.println("Stopping this thread. Current balance: " + bankAccount.getBalance());
 	}
 }
