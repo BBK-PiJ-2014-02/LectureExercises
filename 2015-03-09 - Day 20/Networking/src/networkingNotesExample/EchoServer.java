03ff6c2d00b3d7221debf316d6058da5993a6b47
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-03-09 19:13:54 +0000

    Initial Networking Server example

---
+++ 2015-03-09 - Day 20/Networking/src/networkingNotesExample/EchoServer.java
@@ -0,0 +1,33 @@
+package networkingNotesExample;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class EchoServer extends UnicastRemoteObject implements EchoService {
+	
+	/**
+	 * Serialisation.
+	 */
+	private static final long serialVersionUID = 262271125386487144L;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @throws RemoteException
+	 */
+	public EchoServer() throws RemoteException {
+		// nothing to initialise for this server
+	}
+	
+	/**
+	 * The echo method.
+	 */
+	@Override
+	public String echo(String s) {
+		// This println is not necessary, but helps verifying whether
+		// the server has received the call or not on the remote machine
+		System.out.println("Replied to some client saying '" + s + "'");
+		return s;
+	}
+
+}
